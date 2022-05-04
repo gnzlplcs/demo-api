000a97cf0eceda8b973639e85f4124e9e22ccfb7
gnzlplcs <palaciosgm@outlook.com>
2022-05-04 12:52:51 -0500

    :tada: very small functionality
    style pending

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const url = `https://jsonplaceholder.typicode.com/users`;
+
+  const [ users, setUsers ] = useState();
+
+  const fetchApi = async () => {
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    setUsers(responseJson);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { !users ? 'Loading...' :
+      users.map((user, index) => {
+        return <li key={`${user.name}_${index}`}>{user.name}</li>
+      })}
     </div>
   );
 }
