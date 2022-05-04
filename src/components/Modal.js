1fd7fa6a15e60cdfb917391753034624b59b73e6
gnzlplcs <palaciosgm@outlook.com>
2022-05-04 15:43:56 -0500

    ⬆️ create Modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+import { FaRegWindowClose } from 'react-icons/fa'
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <FaRegWindowClose />
+    </div>
+  )
+}
+
+export default Modal
